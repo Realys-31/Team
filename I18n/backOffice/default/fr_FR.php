862bc73dce21ba08277a62fe609322629b0a0a92
Soldras <apenalver@openstudio.fr>
2016-01-04 11:10:54 +0100

    Translate main page

---
+++ I18n/backOffice/default/fr_FR.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+    "Create a Person" => "Créer une personne",
+    "Create a Team" => "Créer une équipe",
+    "Create a new Team" => "Créer une nouvelle équipe",
+    "Delete a Person" => "Supprimer une personne",
+    "Delete a Team" => "Supprimer une équipe",
+    "Delete this Team" => "Supprimer cette équipe",
+    "Delete this Person" => "Supprimer cette personne",
+    "Do you really want to delete this Person ?" => "Voulez vous supprimer cette personne ?",
+    "Do you really want to delete this Team ?" => "Voulez vous supprimer cette équipe ?",
+    "Modify this Person" => "Modifier cette personne",
+    "Modify a Team" => "Modifier une équipe",
+    "Modify this Team" => "Modifier cette équipe",
+    "Team" => "Équipe",
+    "There is no Person for this Team" => "Il n'y a personne dans cette équipe",
+    "There is no Team" => "Il n'y a aucune équipe",
+);
--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -1,4 +1,4 @@
 <?php
 return array(
-    // 'an english string' => 'La traduction française de la chaine',
+    "Team" => "Équipe",
 );
